371c55eef0cf7a7f15b1fb4e1ad864baf16479ac
Ruyonga Daniel <druyonga@gmail.com>
2020-12-02 10:46:37 +0300

    chore: code cleanup

--- src/main/kotlin/ExpenseReport.kt
+++ src/main/kotlin/ExpenseReport.kt
@@ -1,12 +1,52 @@
 package ru.adventOfCode
 class ExpenseReport {
-    fun  sum(current: Int, next: Int): Int {
-
+    private fun  sum(current: Int, next: Int): Int {
         return  current + next
     }
 
-    fun  sum(base: Int, current: Int, next: Int): Int {
-
+    private fun  sum(base: Int, current: Int, next: Int): Int {
         return  current + next+base
     }
+
+    fun twoNumbersEqual2020(list: List<String>) {
+        val expenseReport = ExpenseReport()
+        val registerSize = list.size
+        var index = 0
+        while (index < registerSize) {
+            val base = Integer.parseInt(list[index])
+            for (item in list) {
+                val is2020 = expenseReport.sum(base, Integer.parseInt(item))
+                if (is2020 == 2020) {
+                    println("index $index base $base + current item $item = $is2020")
+                    println("base * parseInt(item)--------------------------------------------------------------------------")
+                    println(base * Integer.parseInt(item))
+                }
+            }
+            index++
+
+        }
+    }
+
+    fun threeNumbersEqual2020(list: List<String>) {
+        val expenseReport = ExpenseReport()
+        val registerSize = list.size
+        var index = 0
+        var current = 1
+        while (index < registerSize && current < registerSize) {
+            val base = Integer.parseInt(list[index])
+            for (item in list) {
+                val currentItem = Integer.parseInt(item)
+
+                for (next in list) {
+                    val is2020 = expenseReport.sum(base, currentItem, Integer.parseInt(next))
+                    if (is2020 == 2020) {
+                        println("index $index base $base + current $currentItem  item $item = $is2020")
+                        println(base * currentItem * Integer.parseInt(item))
+                    }
+                }
+            }
+            index++
+        }
+    }
+
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,53 +7,13 @@ import java.lang.Integer.parseInt
 fun main() {
     val content = File("./src/main/kotlin/register.txt").readText()
     val list = content.split("\n").map { it.trim() }
-
-    //twoNumbersEqual2020(list)
-    //threeNumbersEqual2020(list)
+//    val expenseReport = ExpenseReport()
+//    expenseReport.twoNumbersEqual2020(list)
+//    expenseReport.threeNumbersEqual2020(list)
     val passwordPolicy = PasswordPolicy()
     val validPasswordSize = passwordPolicy.checkForValidPasswords("./src/main/kotlin/daytwo.txt")
     println(validPasswordSize)
 
 }
 
-fun twoNumbersEqual2020(list: List<String>) {
-    val expenseReport = ExpenseReport()
-    val registerSize = list.size
-    var index = 0
-    while (index < registerSize) {
-        val base = parseInt(list[index])
-        for (item in list) {
-            val is2020 = expenseReport.sum(base, parseInt(item))
-            if (is2020 == 2020) {
-                println("index $index base $base + current item $item = $is2020")
-                println("base * parseInt(item)--------------------------------------------------------------------------")
-                println(base * parseInt(item))
-            }
-        }
-        index++
-
-    }
-}
-
-fun threeNumbersEqual2020(list: List<String>) {
-    val expenseReport = ExpenseReport()
-    val registerSize = list.size
-    var index = 0
-    var current = 1
-    while (index < registerSize && current < registerSize) {
-        val base = parseInt(list[index])
-        for (item in list) {
-            val currentItem = parseInt(item)
-
-            for (next in list) {
-                val is2020 = expenseReport.sum(base, currentItem, parseInt(next))
-                if (is2020 == 2020) {
-                    println("index $index base $base + current $currentItem  item $item = $is2020")
-                    println(base * currentItem * parseInt(item))
-                }
-            }
-        }
-        index++
-    }
-}
 
